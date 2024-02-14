0c7e0a06fdfd88622295bc0ddd0d989af07f6ea5
217099 <daniel.dlugos@me.com>
2024-02-14 13:25:30 +0100

    Pridanie objektu State a BFS funkcie

--- buckets.py
+++ buckets.py
@@ -12,10 +12,51 @@ class Bucktes:
     def print_state(self):
         print("Buckets: ", self.bucket_one, ":", self.bucket_two)
 
+class State():
+    def __init__(self, buckets, previous = None):
+        self.buckets = buckets
+        self.previous = previous
+
+    def compare(self, other_state):
+        if (
+            self.buckets.bucket_one == other_state.buckets.bucket_one 
+            and self.buckets.bucket_two == other_state.buckets.bucket_two
+        ):
+            return True
+        else:
+            return False
+
+def bfs(start, end):
+    start_state = State(start)
+    end_state = State(end)
+
+    open = list()
+    closed = list()
+
+    solution = None 
+    found = False
+
+    open.append(start_state)
+
+    while len(open):
+        state = open.pop(0)
+        closed.append(state)
+
+        if state.compare(end_state):
+            solution = state
+            found = True
+
+            break
+    
+    # backtrack
+
+
 if __name__== '__main__':
     
-    start = Buckets()
+    start = Bucktes()
     end = Bucktes(4,2)
 
     start.print_state()
     end.print_state()
+
+    bfs(start, end)
