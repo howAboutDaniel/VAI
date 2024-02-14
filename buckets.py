ac26db2a7bbc088db0f8e9e7531d247249c49822
217099 <daniel.dlugos@me.com>
2024-02-14 13:45:25 +0100

    Príprava do 3. cvičenia

--- buckets.py
+++ buckets.py
@@ -12,10 +12,21 @@ class Bucktes:
     def print_state(self):
         print("Buckets: ", self.bucket_one, ":", self.bucket_two)
 
+    def fill_bucket_one(self):
+        if self.bucket_one == self.bucket_one_volume:
+            return False
+        else:
+            self.bucket_one = self.bucket_one_volume
+            return True
+
 class State():
     def __init__(self, buckets, previous = None):
         self.buckets = buckets
         self.previous = previous
+
+    def get_rules(self):
+        # 6 možností pre vedierka (buckets)
+        return 6
 
     def compare(self, other_state):
         if (
@@ -25,6 +36,18 @@ class State():
             return True
         else:
             return False
+
+    def expand(self, rule):
+        b = Bucktes(self.buckets.bucket_one, self.buckets.bucket_two)
+        new_state = None
+        valid = False
+
+        if (rule == 0):
+            valid = b.fill_bucket_one()
+
+            if valid:
+                new_state = State(b, self)
+
 
 def bfs(start, end):
     start_state = State(start)
@@ -45,18 +68,17 @@ def bfs(start, end):
         if state.compare(end_state):
             solution = state
             found = True
-
+            print("Riešenie nájdené!")
             break
     
+        for i in range(0, state.get_rules()):
+
+            next_state = state.expand(i)
     # backtrack
 
-
-if __name__== '__main__':
-    
+if __name__ == '__main__':
     start = Bucktes()
     end = Bucktes(4,2)
-
     start.print_state()
     end.print_state()
-
     bfs(start, end)
